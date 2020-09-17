cb0e9de6eea4ae41153731015f6de8d53f75b8fa
Brad Nelson <bjnelson@stanford.edu>
2020-09-17 14:10:57 -0500

    simple python script

---
+++ lectures/01/script.py
@@ -0,0 +1,16 @@
+"""
+A simple python script
+"""
+
+# printing will print to the terminal
+print("hello world")
+
+def f(a, b):
+    """
+    returns the sum of a and b
+    """
+    return a + b
+
+a = 1
+b = 2
+print(f"{a} + {b} = {f(a,b)}")
